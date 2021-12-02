aed44da4fa99cce39d456d7cb4e00ccfb65f67d2
anott03 <amitav_nott@ryecountryday.org>
2021-12-02 14:53:36 -0500

    Revert "Revert "day 1""
    This reverts commit bf34e40c30ec2c410aea4b803c443457ce8826af.

---
+++ day1/target/debug/deps/day1-e150ee3db99b89cb.d
@@ -0,0 +1,5 @@
+/Users/amitavnott/repos/aoc2021/day1/target/debug/deps/day1-e150ee3db99b89cb: src/main.rs
+
+/Users/amitavnott/repos/aoc2021/day1/target/debug/deps/day1-e150ee3db99b89cb.d: src/main.rs
+
+src/main.rs:

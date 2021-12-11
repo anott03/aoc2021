7b5e6c7e6c0b530d845bc4db54ac3b146b3f8143
anott03 <amitav_nott@ryecountryday.org>
2021-12-10 23:32:41 -0500

    day 9 part 1

---
+++ day9/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+
+infile = sys.argv[1] if len(sys.argv) > 1 else 'input'
+
+lines = []
+for line in open(infile):
+    lines.append([int(x) for x in list(line.strip())])
+
+def part1(lines):
+    s = 0
+    candidates = []
+    for i in range(len(lines)):
+        line = lines[i]
+        for j in range(len(line)):
+            if j == 0:
+                if line[j] < line[j+1]:
+                    candidates.append((i, j))
+            elif j == len(line)-1:
+                if line[j] < line[j-1]:
+                    candidates.append((i, j))
+            else:
+                if line[j] < line[j + 1] and line[j] < line[j - 1]:
+                    candidates.append((i, j))
+    
+    for candidate in candidates:
+        i = candidate[0]
+        j = candidate[1]
+        if i == 0:
+            if lines[i+1][j] > lines[i][j]:
+                s += lines[i][j] + 1
+        elif i == len(lines)-1:
+            if lines[i - 1][j] > lines[i][j]:
+                s += lines[i][j] + 1
+        else:
+            if lines[i + 1][j] > lines[i][j] and lines[i-1][j] > lines[i][j]:
+                s += lines[i][j] + 1
+
+    return s
+
+
+print(part1(lines))

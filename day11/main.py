aaa5287211e2b0592e676140d9677248b3455512
anott03 <amitav_nott@ryecountryday.org>
2021-12-11 16:57:12 -0500

    day 11 part 2

--- day11/main.py
+++ day11/main.py
@@ -9,9 +9,10 @@ for line in open(infile):
     X.append([int(x) for x in line.strip()])
 
 ct = 0
-def flash(r, c):
+def flash(r, c, t):
     global ct
-    ct += 1
+    if t <= 100:
+        ct += 1
     X[r][c] = -1
     for dr in [-1, 0, 1]:
         for dc in [-1, 0, 1]:
@@ -20,19 +21,27 @@ def flash(r, c):
             if 0<=rr<len(X) and 0<=cc<len(X[0]) and X[rr][cc] != -1:
                 X[rr][cc] += 1
                 if X[rr][cc] >= 10:
-                    flash(rr, cc)
+                    flash(rr, cc, t)
 
-for t in range(100):
+t = 0
+while True:
+    t += 1
     for r in range(len(X)):
         for c in range(len(X[0])):
             X[r][c] += 1
     for r in range(len(X)):
         for c in range(len(X[0])):
              if X[r][c] == 10:
-                 flash(r, c)
+                 flash(r, c, t)
+    all = True
     for r in range(len(X)):
         for c in range(len(X[0])):
             if X[r][c] == -1:
                 X[r][c] = 0
+            else:
+                all = False
+    if all:
+        print("Part 2: ", t)
+        break
 
-print(ct)
+print("Part 1: ", ct)

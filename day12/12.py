fa464a5680099f5ebc96b6e0d6140aedf2b0c110
anott03 <amitav_nott@ryecountryday.org>
2021-12-12 15:45:13 -0500

    day 12 part 2

--- day12/12.py
+++ day12/12.py
@@ -10,21 +10,23 @@ for line in open(infile):
     N[a].append(b)
     N[b].append(a)
 
-start = ('start', set(['start']))
+start = ('start', set(['start']), None)
 Q = deque([start])
 c = 0
 
+P2 = True  # true for part 2 answer, false for part 1 answer
 while Q:
-    pos, seen = Q.popleft()
+    pos, seen, twice = Q.popleft()
     if pos == 'end':
         c += 1
         continue
     for y in N[pos]:
-        if not y in seen:
+        if y not in seen:
             new_seen = set(seen)
             if y[0].islower():
                 new_seen.add(y)
-            Q.append((y, new_seen))
-    print(Q)
+            Q.append((y, new_seen, twice))
+        elif P2 and y in seen and twice is None and y not in ['start', 'end']:
+            Q.append((y, seen, y))
 
 print(c)
